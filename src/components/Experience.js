967eafb99425b672d77b82e5b3b0be112e4a5d95
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-02-17 22:21:39 -0800

    adds personalized color pallette and background image

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -33,11 +33,11 @@ class Experience extends Component {
             className="vertical-timeline-element--work"
             date={work.years}
             iconStyle={{
-              background: "#AE944F",
+              background: "#FFCE8A",
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
+            icon={<i className="fab fa-code experience-icon"></i>}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
@@ -78,8 +78,8 @@ class Experience extends Component {
             {work}
             <VerticalTimelineElement
               iconStyle={{
-                background: "#AE944F",
-                color: "#fff",
+                background: "#FFCE8A",
+                color: "#FFF",
                 textAlign: "center",
               }}
               icon={
